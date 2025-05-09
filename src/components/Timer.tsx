ad69f4eca9725f7f951d50cfaffcb01589274fef
kei <otaki0413.it@gmail.com>
2025-05-09 13:01:46 +0900

    タイマー機能の実装

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,20 +1,78 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ModeTypeButton } from "./ModeTypeButton";
 import { ControlButton } from "./ControlButton";
 import { Pause, Play, RefreshCcw } from "lucide-react";
+import { TIMER_OPTIONS } from "../constants";
 
 const Timer = () => {
-  // 作業モードと休憩モードの状態を管理
   const [mode, setMode] = useState<"work" | "break">("work");
-  // タイマーの状態を管理
   const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const [pausedTimeRemaining, setPausedTimeRemaining] = useState<number | null>(
+    null
+  );
+  // setIntervalメソッドのIDを管理
+  const intervalRef = useRef<NodeJS.Timeout | undefined>(undefined);
 
-  // ダミーコード（分と秒を表示）
-  const displayMinutes = "25";
-  const displaySeconds = "00";
-  const handleStart = () => {};
-  const handleStop = () => {};
-  const handleReset = () => {};
+  // モード切り替え
+  const handleChangeMode = () => {
+    handleReset();
+    setMode(mode === "work" ? "break" : "work");
+  };
+
+  // タイマー開始
+  const handleStart = () => {
+    // TODO: オーディオ設定
+
+    // 現在時間を格納
+    const currentTime = Date.now();
+    // タイマー停止中、かつ一時停止時の経過時間が存在する場合
+    if (!isRunning && pausedTimeRemaining) {
+      // 一時停止していた時間を引いて、開始時間を補正
+      setStartTime(currentTime - pausedTimeRemaining);
+      setPausedTimeRemaining(null);
+    } else {
+      setStartTime(currentTime);
+    }
+    setNow(Date.now());
+    // 古いタイマー初期化
+    clearInterval(intervalRef.current);
+    // 1秒毎にインターバルIDを更新（現在時刻も更新）
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 1000);
+    setIsRunning(true);
+  };
+
+  // タイマー停止
+  const handleStop = () => {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+    // 停止時点の経過時間を格納
+    if (startTime != null && now != null) {
+      setPausedTimeRemaining(now - startTime);
+    }
+  };
+
+  // タイマーリセット
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
+    setStartTime(null);
+    setNow(null);
+    setIsRunning(false);
+    setPausedTimeRemaining(null);
+  };
+
+  // 経過時間（秒）・・・ミリ秒を秒単位に換算
+  const secondsPassed =
+    startTime != null && now != null ? Math.floor((now - startTime) / 1000) : 0;
+  // 残り時間の秒数（設定時間 - 経過時間）
+  const calculateTime = TIMER_OPTIONS[mode].minutes * 60 - secondsPassed;
+  // 表示時間（分・秒）
+  const displayMinutes = Math.floor(calculateTime / 60);
+  const displaySeconds =
+    calculateTime % 60 < 10 ? "0" + (calculateTime % 60) : calculateTime % 60;
 
   return (
     <div
@@ -32,7 +90,7 @@ const Timer = () => {
               key={modeType}
               modeType={modeType as "work" | "break"}
               currentMode={mode}
-              onClick={() => {}}
+              onClick={handleChangeMode}
             />
           ))}
         </div>
